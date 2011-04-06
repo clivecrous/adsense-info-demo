e4cfcf27d94bc6772689f852d5f5e8b381c08c47
Clive Crous <clive@crous.co.za>
2011-04-06 17:42:38 +0200

    Update demo to use new adsense gem methods

--- adsense-info-demo.rb
+++ adsense-info-demo.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 require 'adsense-info'
 
+raise "GOOGLE_USERNAME must be set in the environment" unless ENV.has_key?( 'GOOGLE_USERNAME' )
+raise "GOOGLE_PASSWORD must be set in the environment" unless ENV.has_key?( 'GOOGLE_PASSWORD' )
+
 ADSENSE = Adsense::Info.new( ENV['GOOGLE_USERNAME'], ENV['GOOGLE_PASSWORD'] )
 
 get '/' do
+  ADSENSE.update_values!
   response['Cache-Control'] = 'public, max-age=60' # Cache for one minute to prevent slamming adsense with requests
   @today_so_far = ADSENSE.today_so_far
+  @yesterday = ADSENSE.yesterday
+  @this_month_so_far = ADSENSE.this_month_so_far
+  @last_month = ADSENSE.last_month
   erb :adsense_info
 end
